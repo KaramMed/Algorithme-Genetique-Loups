78831a75fbbc668db61fdadaf351be790d3a1b7c
Mohamed Karam <47895965+KaramMed@users.noreply.github.com>
2020-04-25 18:46:45 +0000

    Update Loup.py

--- Loup.py
+++ Loup.py
@@ -6,19 +6,7 @@ class Loup:
     def __init__(self,nom,genome):
         self.nom = nom
         self.genome = genome
-        # on crée la couleur selon le genom
-        nb_un = genome.count("1")
-        nb_zero = genome.count("0")
-        if nb_un == 4 :
-            self.couleur = " noir "
-        if nb_un == 3 and nb_zero == 1 :
-            self.couleur = " envers noir "
-        if nb_un == 2 and nb_zero == 2 :
-            self.couleur = " gris "
-        if nb_un == 1 and nb_zero == 3 :
-            self.couleur = " envers blanc "
-        if nb_zero == 4 :
-            self.couleur = " blanc "
+
 
     # methode pour le nb de genes a 0 au genome ( on veut garder les plus blancs )
     def nombre_zero(self):
@@ -27,3 +15,20 @@ class Loup:
     # methode qui change un gene du genome
     def change_gene(self,i,j,val):
         self.genome[i:j] = val
+
+    # methode qui retourne la couleur
+    def couleur(self):
+        # on crée la couleur selon le genom
+        nb_un = self.genome.count('1')
+        nb_zero = self.genome.count('0')
+        if nb_un == 4:
+            couleur = " noir "
+        if nb_un == 3:
+            couleur = " envers noir "
+        if nb_un == 2 and nb_zero == 2:
+            couleur = " gris "
+        if nb_zero == 3:
+            couleur = " envers blanc "
+        if nb_zero == 4:
+            couleur = " blanc "
+        return couleur
